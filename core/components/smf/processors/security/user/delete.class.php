be2b5666003cae5050e148c586602a243799d820
timnboys <timnboys@users.noreply.github.com>
2018-03-13 17:27:02 -0400

    Update delete.class.php

--- core/components/smf/processors/security/user/delete.class.php
+++ core/components/smf/processors/security/user/delete.class.php
@@ -4,7 +4,7 @@ if (!class_exists('modUserDeleteProcessor')) {
 	require MODX_CORE_PATH . 'model/modx/processors/security/user/delete.class.php';
 }
 
-class cmfUserDeleteProcessor extends modUserDeleteProcessor {
+class blestaUserDeleteProcessor extends modUserDeleteProcessor {
 
 	/**
 	 * @return bool
@@ -15,4 +15,4 @@ class cmfUserDeleteProcessor extends modUserDeleteProcessor {
 
 }
 
-return 'cmfUserDeleteProcessor';
+return 'blestaUserDeleteProcessor';
